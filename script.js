e398cf29d1ea374d9e50c1a8222e1aa086063b55
mleblanc94 <leblancmichael94@gmail.com>
2021-05-05 21:05:28 -0400

    changes

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let menu = document.querySelector('fa-bars');
+let menu = document.querySelector('.openMenu');
+let subheader = document.querySelector('.subheader');
+let smallDisplay = document.querySelector('.burgerMenuDiv');
 
 let openMenu = () => {
-    
+    subheader.style.display = 'flex';
+    smallDisplay.style.display = 'none';
 }
 
 
-menu.addEventListener("click", openMenu);
+menu.addEventListener('click', openMenu);
