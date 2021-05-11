8bdaf14471730796b0d8d2fdba0c606cfaf54ea9
mleblanc94 <leblancmichael94@gmail.com>
2021-05-10 20:57:35 -0400

    changes

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 let menu = document.querySelector('.openMenu');
 let subheader = document.querySelector('.subheader');
-let smallDisplay = document.querySelector('.burgerMenuDiv');
+let burgerDisplay = document.querySelector('.burgerMenuDiv');
 let exitOut = document.getElementById('exitOut');
 
 let openMenu = () => {
     subheader.style.display = 'flex';
-    smallDisplay.style.display = 'none';
+    burgerDisplay.style.display = 'none';
 }
 
 let exitBurgerMenu = () => {
     subheader.style.display = 'none';
-    smallDisplay.style.display = 'block';
+    burgerDisplay.style.display = 'block';
 }
+
+
 
 
 menu.addEventListener('click', openMenu);
